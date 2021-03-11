aae76790acdc72ffbee59a649225906d0481421b
TejasChauhan6 <tejaschauhan@hotmail.co.uk>
2021-03-11 16:21:02 +0000

    5.2 5.1

---
+++ functions.py
@@ -0,0 +1,26 @@
+import numpy as np
+import math
+
+def reward(b):
+    global A_list
+    global N
+    global R
+    
+
+    if (np.abs(len(r(b,A_list) != 0) == N):
+        return R
+    else():
+        return 0
+        
+
+def score():
+    global B_list
+    global N
+
+    sum = 0
+
+    for i in range(0,N):
+       sum += s(B_list[i])
+
+    return sum + reward
+    
